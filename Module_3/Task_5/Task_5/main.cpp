b4ce092b75b332246287a212f43d9271e57aefbd
TimofeyTst <yarik.mist@gmail.com>
2023-06-02 14:16:51 +0300

    Переделан под алгоритм Прима

--- Module_3/Task_5/Task_5/main.cpp
+++ Module_3/Task_5/Task_5/main.cpp
@@ -2,8 +2,10 @@
 #include <cassert>
 #include <sstream>
 #include <vector>
+#include <memory>
 #include <utility>
-#include <algorithm>
+#include <queue>
+#include <set>
 
 struct IWeightedGraph {
     virtual ~IWeightedGraph() {}
@@ -12,7 +14,7 @@ struct IWeightedGraph {
 
     virtual int VerticesCount() const = 0;
 
-    virtual std::vector<std::pair<int, std::pair<int, int>>> GetEdges() const = 0;
+    virtual std::vector<std::pair<int, int>> GetNextVertices(int vertex) const = 0;
 };
 
 class WeightedGraph : public IWeightedGraph {
@@ -35,76 +37,60 @@ public:
         return static_cast<int>(adjList.size());
     }
 
-    // Возвращаем weight: { from, to }
-    std::vector<std::pair<int, std::pair<int, int>>> GetEdges() const override {
-        std::vector<std::pair<int, std::pair<int, int>>> edges;
-        for (int from = 0; from < adjList.size(); ++from) {
-            for (const auto& edge : adjList[from]) {
-                int to = edge.first;
-                int weight = edge.second;
-                // Так как ребра хранятся в обе стороны
-                if (from < to) {
-                    edges.push_back({ weight, {from, to} });
-                }
-            }
-        }
-        return edges;
+    std::vector<std::pair<int, int>> GetNextVertices(int vertex) const override {
+        return adjList[vertex];
     }
 
 private:
     std::vector<std::vector<std::pair<int, int>>> adjList;
 };
 
-class DSU {
-public:
-    DSU(int size) : parent(size) {
-        for (int i = 0; i < size; ++i) {
-            parent[i] = i;
-        }
-    }
-
-    int FindParent(int v) {
-        if (v == parent[v]) {
-            return v;
-        }
-        return FindParent(parent[v]);
-    }
-
-    void Union(int v1, int v2) {
-        int p1 = FindParent(v1);
-        int p2 = FindParent(v2);
-        if (p1 != p2) {
-            parent[p2] = p1;
-        }
-    }
-
-private:
-    std::vector<int> parent;
-};
-
 int getMinTree(const IWeightedGraph& graph) {
     int vertexCount = graph.VerticesCount();
-    std::vector<std::pair<int, std::pair<int, int>>> edges = graph.GetEdges();
 
-    // Сортируем ребра по весу
-    std::sort(edges.begin(), edges.end());
+    std::vector<bool> visited(vertexCount, false);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+    std::set<int> includedVertices;
 
-    DSU ds(vertexCount);
-    int minSpanningTreeWeight = 0;
+    // Начинаем с первого
+    int startVertex = 0;
+    visited[startVertex] = true;
+    includedVertices.insert(startVertex);
 
-    // Проходим по отсортированным ребрам и добавляем их в остовное дерево, если они не создают цикл
-    for (const auto& edge : edges) {
-        int weight = edge.first;
-        int from = edge.second.first;
-        int to = edge.second.second;
+    // Добавляем начальные вершины в очередь с приоритетом
+    for (const auto& edge : graph.GetNextVertices(startVertex)) {
+        int to = edge.first;
+        int weight = edge.second;
+        pq.push({ weight, to });
+    }
 
-        if (ds.FindParent(from) != ds.FindParent(to)) {
-            ds.Union(from, to);
-            minSpanningTreeWeight += weight;
+    int minWeight = 0;
+
+    while (!pq.empty()) {
+        auto currentEdge = pq.top();
+        pq.pop();
+        int weight = currentEdge.first;
+        int vertex = currentEdge.second;
+
+        // Пропускаем если уже был
+        if (includedVertices.count(vertex) > 0)
+            continue;
+
+        // Иначе добавляем
+        includedVertices.insert(vertex);
+        minWeight += weight;
+        visited[vertex] = true;
+
+        for (const auto& edge : graph.GetNextVertices(vertex)) {
+            int to = edge.first;
+            int weight = edge.second;
+            if (!visited[to]) {
+                pq.push({ weight, to });
+            }
         }
     }
 
-    return minSpanningTreeWeight;
+    return minWeight;
 }
 
 void run(std::istream& input, std::ostream& output) {
@@ -131,16 +117,16 @@ void test() {
     {
         std::stringstream input;
         std::stringstream output;
-        input << "4 4\n1 2 1\n2 3 2\n3 4 5\n4 1 4";
+        input << "5 10\n4 3 3046\n4 5 90110\n5 1 57786\n3 2 28280\n4 3 18010\n4 5 61367\n4 1 18811\n4 2 69898\n3 5 72518\n3 1 85838\n";
         run(input, output);
-        assert(output.str() == "7\n");
+        assert(output.str() == "107923\n");
     }
     {
         std::stringstream input;
         std::stringstream output;
-        input << "5 10\n4 3 3046\n4 5 90110\n5 1 57786\n3 2 28280\n4 3 18010\n4 5 61367\n4 1 18811\n4 2 69898\n3 5 72518\n3 1 85838\n";
+        input << "4 4\n1 2 1\n2 3 2\n3 4 5\n4 1 4";
         run(input, output);
-        assert(output.str() == "107923\n");
+        assert(output.str() == "7\n");
     }
 }
 
